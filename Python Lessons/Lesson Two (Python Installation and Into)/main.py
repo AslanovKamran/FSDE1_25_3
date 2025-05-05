0907fc9fdcf4d8b36bacfe317d32cfba6cfedc58
Aslan17 <k.aslan1707@gmail.com>
2025-05-05 23:49:19 +0400

    Init

---
+++ Python Lessons/Lesson Two (Python Installation and Into)/main.py
@@ -0,0 +1,129 @@
+# print("Hello, world")
+
+# def greet(name):
+#     print("Hello", name)
+
+
+# greet("Aslan")
+
+
+# def cube(number):
+#     print(number * number * number)
+
+
+# print("Nachalo funckii, peredayu chislo 5")
+# cube(-5)
+# print("Konec funkcii")
+
+# weekdays = ["Sunday", "Monday", "Tuesday",
+#             "Wednesday", "Thursday", "Friday", "Saturday"]
+# underEighteen = False
+# age = 17
+# print(weekdays)
+# print(underEighteen)
+# print(age)
+
+# print("Hello")  print("World") # Wrong Syntax
+
+# #Correct Syntax
+# print("Hello,")
+# print("World!")
+
+# print("Hello\nWorld!")
+
+
+# print("The book is called \"The War and peace\"")
+# print("The book is called \'The War and peace\'")
+# print("Tab     test")
+# print("Tab\ttest")
+
+
+# print("Hello", "From", "The", "Other", "Side", sep="🍕🍕🍕")
+
+# print("Hello", end=".\n")
+# print("World", end="!")
+
+# print("2")
+# print(2)
+
+# print(type("2"))
+# print(type(2))
+# print(type(2.5))
+
+# print(8_250_423_613)
+
+
+# print(2,4)
+
+# print(0.4)
+# print(.4)
+
+
+# print(type(4.0))
+# print(type(4))
+
+# print(5+9)
+
+# print("Hello" * 3)
+
+# print(2**2.5)
+
+# print(2**3)
+# print(2**-3)
+
+# print(6 // 4)
+# print(6. // 4)
+
+# print(-4 + 4)
+# print(-4. + 8)
+
+# print(9 % 6 % 2)
+
+# print(2+2*5)
+# print((2+2)*5)
+
+
+# false = "John"
+# print(false)
+
+# age = 17
+# print(age)
+
+
+# name = "Kamran"
+# age = 25
+# work = "IT Step Academy"
+
+
+# name = "Kamran"
+# age = 17
+
+# print(f'Hi, my name is {name}. I am {age} years old')
+
+
+# print("Input your name here...")
+# name = input()
+# print(f"Nice to meet you, {name}")
+
+
+# name = input("Input your name here => ")
+# print(f"Nice to meet you, {name}")
+
+# prompt = input("Enter a number: ")
+# result = prompt * 2
+# print(f"Result is {result}")
+
+# prompt = int(input("Enter a number: "))
+# result = prompt * 2
+# print(f"Result is {result}")
+
+
+# prompt = input("Enter a number: ")
+# result = int(prompt) * 2
+# print(f"Result is {result}")
+
+number = 12345
+lastNumber = number % 10
+print(lastNumber)
+preLastNumber = (number // 10) % 10
+print(preLastNumber)
