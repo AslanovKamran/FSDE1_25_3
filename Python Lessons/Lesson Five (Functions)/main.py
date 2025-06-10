c972a5304964d61f6eda21621893e3a9fb6c5967
Aslan17 <k.aslan1707@gmail.com>
2025-06-10 14:07:08 +0400

    test

--- Python Lessons/Lesson Five (Functions)/main.py
+++ Python Lessons/Lesson Five (Functions)/main.py
@@ -1,9 +1,5 @@
-def getThePow(base, ceiling):
-    print(base ** ceiling)
+import math
 
-
-getThePow(base=5, ceiling=2)  # 25
-getThePow(ceiling=2, base=5)  # 25
-
-getThePow(5, 2)  # 25
-getThePow(2, 5)  # 32
+print(math.pi)
+print(math.tau)
+print(math.e)
