03cbda5548ad1c2c626751defebb2c62dd3fcb08
Aslan17 <k.aslan1707@gmail.com>
2025-07-06 01:13:15 +0400

    Exceptions added

--- Python Lessons/Lesson Six (Exceptions)/main.py
+++ Python Lessons/Lesson Six (Exceptions)/main.py
@@ -1,10 +1,17 @@
-try:
-    # код, где может возникнуть ошибка
-    x = int(input("Введите число: "))
-    print(10 / x)
-except Exception:
-    print("I catched the most common exception. I don't know what it was about but I catched it")
-except ZeroDivisionError:
-    print("На ноль делить нельзя!")
-except ValueError:
-    print("Нужно ввести число.")
+def validate_age(age):
+    if age < 0:
+        raise ArithmeticError("Возраст не может быть отрицательным.")
+    print(f"Возраст принят: {age}")
+
+
+def get_user_age():
+    try:
+        age = int(input("Введите возраст: "))
+        validate_age(age)
+    except ArithmeticError as e:
+        print(f"Ошибка: {e}")
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+
+
+get_user_age()
